3d9b5e5f3f65e2638243c447649cc5fa31455846
NightZan999 <nigamanth.srivatsan@gmail.com>
2021-01-08 10:05:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server Up!")
+})
